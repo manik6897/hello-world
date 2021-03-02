73185fe33e44fcd4187b33854b100a59964e42e2
manik6897 <74670285+manik6897@users.noreply.github.com>
2021-03-01 22:04:12 -0500

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,9 @@
+  function formSubmit() {
+    document.forms["myForm"].submit();
+
+ function formul()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("Output is: ").innerHTML = num1 * num2;
+}
